3a05069164b440ed002ba38382e429cff555a948
Mauro Andreolini <mauro.andreolini@gmail.com>
2012-12-14 13:38:55 +0100

    full-trace.sh: bug fix in write_trace()
    One of the operations in write_trace() does refer to the trace file
    correctly; it uses the "/tmp/trace" static name instead of the correct
    $1 parameter.

--- full-trace.sh
+++ full-trace.sh
@@ -144,7 +144,7 @@ trace_process() {
 write_trace() {
 	cat /sys/kernel/debug/tracing/trace > $1
 	sed -i -e '1,/PROCESS/ d;' $1
-	last=$(grep -nE "$2-[[:digit:]]+\s+=>" /tmp/trace | tail -n 1 | cut -f1 -d:)
+	last=$(grep -nE "$2-[[:digit:]]+\s+=>" $1 | tail -n 1 | cut -f1 -d:)
 	last=$((last+1))
 	sed -i -e "$last,$ d;" $1
 }
