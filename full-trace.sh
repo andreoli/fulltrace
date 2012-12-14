520f683ba998389509e1782b014ac53f6663eb12
Arianna Avanzini <avanzini.arianna@gmail.com>
2012-12-14 14:03:15 +0100

    full-trace.sh: trim program basename in write_trace() to seven characters
    The write_trace() function uses the program basename to find the
    last occurrence of the program name in a context switch. Unfortunately,
    that same name is trimmed to seven characters in the trace output.
    As a consequence, grep is unable to find the last occurrence of the
    program if its name is longer than seven characters. The following
    commit fixes the problem, using only the first seven characters of
    the program name in the comparison.

--- full-trace.sh
+++ full-trace.sh
@@ -144,7 +144,8 @@ trace_process() {
 write_trace() {
 	cat /sys/kernel/debug/tracing/trace > $1
 	sed -i -e '1,/PROCESS/ d;' $1
-	last=$(grep -nE "$2-[[:digit:]]+\s+=>" $1 | tail -n 1 | cut -f1 -d:)
+	shortname=${2:0:7}
+	last=$(grep -nE "${shortname}-[[:digit:]]+\s+=>" $1 | tail -n 1 | cut -f1 -d:)
 	last=$((last+1))
 	sed -i -e "$last,$ d;" $1
 }
